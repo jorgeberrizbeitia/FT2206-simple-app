623e216ad26dcf0e48b693bd15c58b40457c344b
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-07-22 15:16:18 +0200

    half done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,30 @@
-const router = require("express").Router();
+const express = require("express")
+const router = express.Router();
+const Pokemon = require("../models/Pokemon.model.js")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/pokemons", (req, res, next) => {
+
+  // .find()
+  // Model
+  Pokemon.find()
+  .then((response) => {
+    console.log(response)
+    // let pokemonArr = response
+    res.render("pokemon/list.hbs", {
+      pokemonArr: response
+    })
+
+  })
+  .catch((err) => {
+    next(err)
+  })
+
+
+})
 
 module.exports = router;
--- seeds/pokemon.seed.js
+++ seeds/pokemon.seed.js
@@ -3,8 +3,21 @@
 // 1 conectarnos a la BD
 require("../db") // reutiliza la connection de la DB de nuestro server
 
-// 2 buscar los documentos a agregar
+// 2 obtener los documentos a agregar a la DB
 const pokemonArr = require("./pokemon.json")
 
 // 3 agregar los documentos a la DB
-// ... tener un Modelo
+// ... tener un Modelo inserMAny
+const Pokemon = require("../models/Pokemon.model.js")
+
+async function storePokemon() {
+  try {
+    await Pokemon.insertMany(pokemonArr)
+    console.log("todo bien, los pokemon fueron agregados a la BD")
+    
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+storePokemon()
